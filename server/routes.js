e47946c6246c3d0050193b849415672c4a91b675
Wanlin He <katyhe@sas.upenn.edu>
2023-04-21 04:26:08 -0400

    rec optimized

--- server/routes.js
+++ server/routes.js
@@ -195,7 +195,7 @@ const two_degree = async function(req, res) {
 
   connection.query(
     `WITH D0 AS (
-      SELECT curr_following 
+      SELECT following 
       FROM Following F
       WHERE F.user = '${currUser}'	
     )
@@ -203,7 +203,7 @@ const two_degree = async function(req, res) {
     FROM Following F1 
     JOIN Following F2 ON F1.following = F2.user
     WHERE F1.user = '${currUser}' AND F2.following <> '${currUser}' AND F2.following NOT IN (
-    SELECT curr_following 
+    SELECT following 
     FROM D0
     ) )
     UNION
@@ -212,7 +212,7 @@ const two_degree = async function(req, res) {
     JOIN Following F2 ON F1.following = F2.user
     JOIN Following F3 ON F2.following = F3.user
     WHERE F1.user = '${currUser}' AND F2.following <> '${currUser}' AND F3.following <> '${currUser}' AND F3.following NOT IN (
-    SELECT curr_following 
+    SELECT following 
     FROM D0
     ) )`, (err, data) => {
       if (err || data.length === 0) {
@@ -246,129 +246,205 @@ const top_genre = async function(req, res){
     });
 }
 
+//Route 9.1: POST /rec/:user_id
+//need to be tested
+const create_views = async function(req, res){
+  const currUser = req.params.user_id 
+  
+  connection.query(
+    `CREATE VIEW T1_${currUser} AS(
+      WITH AllMovie AS (
+          SELECT distinct movie_id
+          FROM MovieRatings
+      ),
+      RatedByUser AS (
+          SELECT movie_id, rating
+          FROM MovieRatings
+          WHERE username = '${currUser}'
+      )
+      SELECT AM.movie_id,
+      CASE
+          WHEN rating IS NULL then 0
+          ELSE rating
+      END AS rating
+      FROM AllMovie AM LEFT JOIN RatedByUser RU on RU.movie_id = AM.movie_id
+    );
+
+    CREATE VIEW T2_${currUser} AS (
+      WITH AllMovie AS (
+        SELECT distinct movie_id
+        FROM MovieRatings
+      ),
+      RatedByUser AS (
+        (SELECT username, movie_id as id, rating
+         FROM MovieRatings
+         WHERE username in (SELECT potentials FROM TopFollowed)
+         )
+         UNION
+         (
+            SELECT username, movie_id as id, rating
+            FROM MovieRatings
+            WHERE username in (SELECT potential FROM TwoDegree_${currUser})
+        )
+      )
+    SELECT AM.movie_id as id, username,
+    CASE
+        WHEN RU.rating IS NULL THEN 0
+        ELSE RU.rating
+    END AS rating
+    FROM AllMovie AM
+    LEFT JOIN RatedByUser RU ON AM.movie_id = RU.id);`, (err, data) =>{
+      if (err) {
+        console.log(err);
+        res.status(400).send(`Views cannot be added to the database. Please try again`);
+      } else {
+        res.status(200).send(`Views Created`);
+      }
+    }
+  )
+}
 
 //Route 9: GET /rec/:user_id
-//not tested
-// const rec = async function(req, res){
-//   const currUser = req.params.user_id 
+//tested
+const rec = async function(req, res){
+  const currUser = req.params.user_id 
 
-//   connection.query(
-//     `
-//     CREATE VIEW TwoDegree AS (
-//       WITH D0 AS (
-//         SELECT curr_following 
-//         FROM Following F
-//         WHERE F.user = '${currUser}'	
-//       )
-//       (SELECT DISTINCT F2.following AS potential
-//       FROM Following F1 
-//       JOIN Following F2 ON F1.following = F2.user
-//       WHERE F1.user = '${currUser}' AND F2.following <> '${currUser}' AND F2.following NOT IN (
-//       SELECT curr_following 
-//       FROM D0
-//       ) )
-//       UNION
-//       (SELECT DISTINCT F3.following AS potential
-//       FROM Following F1 
-//       JOIN Following F2 ON F1.following = F2.user
-//       JOIN Following F3 ON F2.following = F3.user
-//       WHERE F1.user = '${currUser}' AND F2.following <> '${currUser}' AND F3.following <> '${currUser}' AND F3.following NOT IN (
-//       SELECT curr_following 
-//       FROM D0
-//       ) ) 
-//     )
-
-//     CREATE VIEW T1 AS(
-//       WITH AllMovie AS (
-//           SELECT distinct id
-//           FROM Movies
-//       ),
-//       RatedByUser AS (
-//           SELECT movie_id, rating
-//           FROM MovieRatings
-//           WHERE username = '${currUser}'
-//   )
-//       SELECT id,
-//       CASE
-//           WHEN rating IS NULL then 0
-//           ELSE rating
-//       END AS rating
-//       FROM AllMovie AM LEFT JOIN RatedByUser RU on RU.movie_id = AM.id
-//     )
-
-//     CREATE VIEW TopFollowed As (
-//       SELECT DISTINCT user as potentials, COUNT(follower) as count
-//       FROM Follower
-//       GROUP BY user
-//       ORDER BY count DESC
-//       LIMIT 20
-//     )
-  
-//     CREATE VIEW T2 AS (
-//       WITH AllMovie AS (
-//           SELECT distinct id
-//           FROM Movies
-//       ),
-//       RatedByUser AS (
-//           SELECT username, movie_id as id, rating
-//           FROM MovieRatings
-//           WHERE username in (SELECT potentials FROM TwoDegree, TopFollowed)
-//       )
-//       SELECT AM.id as id, username,
-//       CASE
-//           WHEN (AM.id, username) not in (SELECT id, username FROM RatedByUser) THEN 0
-//           ELSE rating
-//       END AS rating
-//       FROM AllMovie AM, RatedByUser RU
-//     )
-  
-//     WITH DotProduct AS (
-//     SELECT T2.username as user,
-//     SUM(T1.rating * T2.rating) / (SQRT(SUM(T1.rating * T1.rating)) * SQRT(SUM(T2.rating * T2.rating))) as Dot_Product
-//     FROM T1, T2
-//     GROUP BY T2.username
-//     )
-//     SELECT user
-//     FROM DotProduct
-//     ORDER BY Dot_Product DESC
-//     LIMIT 20
-
-//     DROP VIEW T1
-//     DROP VIEW TopFollowed
-//     DROP VIEW T2
-//     `, (err, data) => {
-//       if (err || data.length === 0) {
-//         console.log(err);
-//         res.json({});
-//       } else {
-//         res.json(data);
-//       }
-//     } 
-//   )
-// } 
+  connection.query(
+    `WITH DotProduct AS (
+      SELECT T2_${currUser}.username as user,
+      SUM(T1_${currUser}.rating * T2_${currUser}.rating) / (SQRT(SUM(T1_${currUser}.rating * T1_${currUser}.rating)) * SQRT(SUM(T2_${currUser}.rating * T2_${currUser}.rating))) as Dot_Product
+      FROM T1_${currUser} JOIN T2_${currUser} ON T1_${currUser}.movie_id = T2_${currUser}.id
+      GROUP BY T2_${currUser}.username
+    )
+  SELECT user
+  FROM DotProduct
+  ORDER BY Dot_Product DESC
+  LIMIT 10`, (err, data) => {
+      if (err || data.length === 0) {
+        console.log(err);
+        res.json({});
+      } else {
+        res.json(data);
+      }
+    } 
+  )
+} 
 
 
-// // Route 11: POST /register
-// // not tested
-// const register = async function(req,res){
-//   const genre_pref_2 = req.params.genre_pref_1 ?? '%';
-//   const genre_pref_3 = req.params.genre_pref_3 ?? '%';
-//   connection.query(
-//     `INSERT INTO Users (username, password, genre_pref_1, genre_pref_2, genre_pref_3)
-//     VALUES ('${req.params.user_id}', '${req.params.password}','${req.params.genre_pref_1}','${genre_pref_2}','${genre_pref_3}')
-//     `, (err, data) => {
-//       if (err || data.length === 0) {
-//         console.log(err);
-//         res.json({});
-//       } else {
-//         res.json(data);
-//       }
-//     });
-// }
+// Route 11: POST /register
+// not tested
+const register = async function(req,res){
+  const genre_pref_2 = req.params.genre_pref_2 ?? '';
+  const genre_pref_3 = req.params.genre_pref_3 ?? '';
+  connection.query(
+    `SELECT username FROM Users WHERE username = '${req.params.user_id}'`
+    , async(err, data) => {
+      if (err) {
+        console.log(err);
+        res.status(400).send("User Info cannot be added to the database. Please try again");
+      } else if (data.length != 0){
+        console.log(`Useranme exists`);
+        res.status(409).send(`Username already exists`);
+      }else {
+        connection.query(
+        `INSERT INTO Users (username, password, genre_pref_1, genre_pref_2, genre_pref_3)
+        VALUES ('${req.params.user_id}', '${req.params.password}','${req.params.genre_pref_1}','${genre_pref_2}','${genre_pref_3}')
+        `, async (err, data) => {
+          if (err) {
+            console.log(err);
+            res.status(400).send(`User Info cannot be added to the database. Please try again`);
+          } else {
+            res.status(200).send(`User Registered`);
+          }
+        });
+      }
+    });
+}
 
+//Route 12: POST /rate_movie/:movie_id
+const rate_movie = async function(req,res){
+  const timestamp = new Date();
+  const usrComment = req.params.comment ?? '';
+  connection.query(
+    `INSERT INTO MovieRatings (username, movie_id, rating, comment, timestamp)
+    VALUES ('${req.params.user_id}', '${req.params.movie_id}','${req.params.rating}','${usrComment}','${timestamp}')
+    `, (err, data) => {
+      if (err) {
+        console.log(err);
+        res.status(400).send(`User cannot add a comment to the database. Please try again`);
+      } else {
+        res.status(201).send(`User '${req.params.user_id}' created a new comment`)
+      }
+    });
+}
 
+//Route 13: /delete_rating/:movie_id
+const delete_rating = async function(req,res){
+  const currusr = req.params.user_id;
+  const movieid = req.params.movie_id;
+  connection.query(
+    `DELETE FROM MovieRatings WHERE username ='${currusr}' AND movie_id = '${movieid}'
+    `, (err, data) => {
+      if (err) {
+        console.log(err);
+        res.status(400).send(`User cannot delete a comment to the database. Please try again`);
+      } else {
+        res.status(200).send(`User '${currusr}' deleted rating for '${movieid}'`)
+      }
+    });
+}
 
+//Route 14: /new_follow
+const new_follow = async function(req,res){
+  const currusr = req.params.user_id;
+  const followid = req.params.follow_id;
+  connection.query(
+    `INSERT INTO Follower
+     VALUES ('${currusr}', '${followid}')
+    `, async (err, data) => {
+      if (err) {
+        console.log(err);
+        res.status(400).send(`Follow unsuccessful. Please try again`);
+      } else {
+        connection.query(
+          `INSERT INTO Following
+          VALUES ('${followid}','${currusr})`, async (err, data) => {
+          if (err) {
+            console.log(err);
+            res.status(400).send(`Follow unsuccessful. Please try again`);
+          } else {
+            res.status(201).send(`'${followid}' now follows you`)
+          }
+        });
+        
+      }
+    });
+}
 
+//Route 15: /delete_follow
+const delete_follow = async function(req,res) {
+  const currusr = req.params.user_id;
+  const followid = req.params.follow_id;
+  connection.query(
+    `DELETE FROM Follower WHERE username = '${currusr} AND follower = '${followid}
+    `, async (err, data) => {
+      if (err) {
+        console.log(err);
+        res.status(400).send(`Follow unsuccessful. Please try again`);
+      } else {
+        connection.query(
+          `INSERT INTO Following WHERE following = '${followid}' AND username = '${currusr}'
+          `, async (err, data) => {
+          if (err) {
+            console.log(err);
+            res.status(400).send(`Unfollow unsuccessful. Please try again`);
+          } else {
+            res.status(201).send(`You have now unfollowed'${followid}'`)
+          }
+        });
+      }
+    });
+}
 
 module.exports = {
   trending,
@@ -378,5 +454,12 @@ module.exports = {
   update,
   top_movies,
   two_degree,
-  top_genre
+  top_genre,
+  rec,
+  create_views,
+  register,
+  rate_movie,
+  delete_rating,
+  new_follow,
+  delete_follow
 }
